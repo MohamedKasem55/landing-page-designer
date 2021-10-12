1fac0f75c0c5935d8fe51d06a038a2cc71891868
MohamedKasem55 <81971922+MohamedKasem55@users.noreply.github.com>
2021-10-12 14:58:38 +0200

    update

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
  */
 /* import Button from 'react-bootstrap/Button';
  */
-import Test from "./test";
 import "./App.css";
 import Colors from "./colors";
 import {
